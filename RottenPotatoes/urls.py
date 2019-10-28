107f912b1640e7bf3e2b6d2b9687d808122c58fb
unknown <bjcahill@calpoly.edu>
2019-10-27 20:46:19 -0700

    added Google sign in and home page template

--- RottenPotatoes/urls.py
+++ RottenPotatoes/urls.py
@@ -18,7 +18,20 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+
+from django.conf.urls import url
+
+from users.views import logout
+
+from django.contrib.auth import login
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('users/', include('users.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path('', include('pages.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='polls'),
     path('movie/details/<slug:id>/', views.details, name='details')
 ]
