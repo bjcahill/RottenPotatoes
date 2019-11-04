0c87758bdab2680938e1c762c617ac9e3615d84c
unknown <bjcahill@calpoly.edu>
2019-11-03 17:42:01 -0800

    added bare-bone user profiles w/ html and backend

--- users/models.py
+++ users/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+import psycopg2
 
 # Create your models here.
 class Usermodel(models.Model):
-   user_name = models.CharField(max_length = 100)
+   user_name = models.CharField(max_length = 100,default="NULL")
+   first_name = models.CharField(max_length = 100,default="NULL")
+   last_name = models.CharField(max_length = 100,default="NULL")
+   average_rating = models.FloatField(default = 0)
    join_date = models.DateTimeField('date joined')
+   user = models.ForeignKey('auth.User', default=1, on_delete=models.SET_DEFAULT)
+   image = models.ImageField(upload_to='images', blank=True)
 
    def __str__(self):
       return self.user_name
