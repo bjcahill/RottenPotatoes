9b5d95011b72b6c8e37d46417e81b1c2a5d5c217
mikeford2424 <38016281+mikeford2424@users.noreply.github.com>
2019-12-06 00:13:13 -0800

    more review2 unit tests

--- movies/tests.py
+++ movies/tests.py
@@ -89,15 +89,33 @@ class ReviewTests(TestCase):
         self.assertEqual(review.review, "hello")
         self.assertEqual(review.movie.title, "Pika1")
 
+class UserTests(TestCase):
+    def setUp(self):
+        u = User.objects.create_user('joe', 'lennon@thebeatles.com', 'johnpassword')
+        
+    def test1(self):
+        User.objects.login(u)
+        
+    def test2(self):
+        User.objects.logut(u)
+
 class Review2Tests(TestCase):
     def setUp(self):
         Movie.objects.create(title="Pika1")
-        u = User.objects.create_user('joe', 'lennon@thebeatles.com', 'johnpassword')
+        Movie.objects.create(title = "hi")
+        user1 = User.objects.create_user('joe', 'lennon@thebeatles.com', 'johnpassword')
+        user2 = User.objects.create_user('mike', 'mike@gmail.com', 'mikey')
         Review2.objects.create(id = "1", critic="Joe",
                               review="hello",
                               score = "8.7",
                               movie=Movie.objects.get(title="Pika1"),
-                              user = u,
+                              user = user1,
+                              review_type = True)
+        Review2.objects.create(id = "2", critic="Mike",
+                              review="what's up",
+                              score = "2",
+                              movie=Movie.objects.get(title="hi"),
+                              user = user2,
                               review_type = True)
     def test1(self):
         review = Review2.objects.get(critic="Joe")
@@ -116,3 +134,21 @@ class Review2Tests(TestCase):
         self.assertNotEqual(review.score , 8.8)
         self.assertNotEqual(review.user , "x")
         self.assertNotEqual(review.review_type , False)
+        
+    def test3(self):
+        review = Review2.objects.get(critic="Mike")
+        self.assertNotEqual(review.critic, "Mike2")
+        self.assertNotEqual(review.review, "hello2")
+        self.assertNotEqual(review.movie.title, "Pika12")
+        self.assertNotEqual(review.score , 8.8)
+        self.assertNotEqual(review.user , "x")
+        self.assertNotEqual(review.review_type , False)
+        
+    def test4(self):
+        review = Review2.objects.get(critic="Mike")
+        self.assertEqual(review.critic, "Mike")
+        self.assertEqual(review.review, "what's up")
+        self.assertEqual(review.movie.title, "hi")
+        self.assertEqual(review.score , 2)
+        self.assertEqual(review.user , User.objects.get(username = 'mike'))
+        self.assertEqual(review.review_type , True)
